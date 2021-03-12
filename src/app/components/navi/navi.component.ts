302724c65fa2e7d25b4ae054defa5104d7b1589f
Metheal <metheal@outlook.com>
2021-03-12 21:23:05 +0300

    navbar iyilestirildi

--- src/app/components/navi/navi.component.ts
+++ src/app/components/navi/navi.component.ts
@@ -3,13 +3,35 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navi',
   templateUrl: './navi.component.html',
-  styleUrls: ['./navi.component.css']
+  styleUrls: ['./navi.component.css'],
 })
 export class NaviComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
+  components: string[] = [
+    'Araclar',
+    'Markalar',
+    'Renkler',
+    'Musteriler',
+    'Kiralamalar',
+  ];
+  routes: string[] = ['cars', 'brands', 'colors', 'customers', 'rentals'];
+  pages = new Map();
 
   ngOnInit(): void {
+    this.makeMap(this.components, this.routes);
+    this.fun();
+  }
+
+  makeMap(components: string[], routes: string[]): void {
+    for (let i = 0; i < routes.length; i++) {
+      this.pages.set(components, routes);
+    }
+  }
+  fun() {
+    for (let i = 0; i < this.components.length; i++) {
+      console.log(i);
+    }
+    console.log(this.pages.get(this.components)[0])
   }
 
 }
